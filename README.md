f95cb59120918d303118eb34a625d4fac73e498c
rmelendez <rmelendez@mobilize.net>
2022-08-26 15:28:20 -0600

    Added env.example

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Steps
 3. Uncomment the content in the env file in the environment you like
 3. Open a Comand Line and type the following command
     3.1. **yarn ts-node index.ts**
-4. Script will ask you where would you like to store the pvc, type a valid path
+4. Script will ask you where would you like to store the pvc, type a valid path, example **C:/Users/rmelendez/Desktop/Test PVC**
 
 
 
--- index.ts
+++ index.ts
@@ -1,17 +1,8 @@
 import { downloadFromAzureStorage } from "./src/azure-storage-file-share/download-file-share";
-//import readline from 'readline'
 import { userQuestion } from "./src/utils/ui";
-require('dotenv').config()
+require("dotenv").config();
 
-
-/*var rl = readline.createInterface(process.stdin, process.stdout);
-
-rl.setPrompt(`Write the full path to donwload all the PVC of the environment? `);
-rl.prompt();
-rl.on('line', (path) => {
-console.log(`Writting the PVC in: ${path}`);
-rl.close();
-downloadFromAzureStorage(path)
-});*/
-
-userQuestion('Write the full path to donwload all the PVC of the environment', downloadFromAzureStorage)
+userQuestion(
+  "Write the full path to donwload all the PVC of the environment",
+  downloadFromAzureStorage
+);
